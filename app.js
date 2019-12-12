337bc950ecf95e96a71cc0d28cdc4bb52cad393b
Petra Pogacsas <petra.pogacsas@gmail.com>
2019-12-12 10:58:20 +0000

    update app.js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  mongoose = require('mongoose'),
+  Car = require('./api/models/carsModel'), //created model loading here
+  bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Carsdb'); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/carsRoutes'); //importing route
+
+routes(app); //register the route
+
 app.listen(port);
-console.log('todo list RESTful API server started on: ' + port);
+console.log('Cars API server started on: ' + port);
